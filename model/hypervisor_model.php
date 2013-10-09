296d2a88892bf634ce722a831479a36458db3f0d
mmallett <mmallett91@gmail.com>
2013-10-09 01:10:46 -0500

    mock machines

--- model/hypervisor_model.php
+++ model/hypervisor_model.php
@@ -5,14 +5,38 @@ class Hypervisor_Model{
 	public function __construct(){}
 
 	private $machines = array(
-
-		// name
-		// status
-		// time remaining if on
-		// ip address if on
-
-
+		array(
+			'name' => 'matt_attack',
+			'status' => 'on',
+			'time_remaining' => '16:16:16',
+			'ip_address' => '1.2.3.4',
+			'owner' => 'matt'
+		),
+		array(
+			'name' => 'matt_client',
+			'status' => 'off',
+			'owner' => 'matt'
+		),
+		array(
+			'name' => 'matt_usrp',
+			'status' => 'not imaged',
+			'owner' => 'matt'
+		)
 	);
+
+	public function get_machines_for_user($user){
+		
+		$ret = array();
+
+		foreach($machine in $this->machines){
+			if(machine['owner'] == $user){
+				array_push($ret, machine);
+			}
+		}
+
+		return $ret;
+	}
+
 
 
 
