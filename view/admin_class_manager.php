1e005f2a8fc381abd3ca905c5529f67a571c66c3
Mathew Mallett <mmallett@iastate.edu>
2013-10-23 23:40:53 -0500

    semicolons tho

--- view/admin_class_manager.php
+++ view/admin_class_manager.php
@@ -65,7 +65,7 @@
 				<p>Select Virtual Machines to grant class access to:</p>
 				<?php
 				foreach($data['base_images'] as $image){
-					echo '<div class="checkbox"><label><input type="checkbox" value="' .  $image . '">[' . $image . ']</label></div>\n'
+					echo '<div class="checkbox"><label><input type="checkbox" value="' .  $image . '">[' . $image . ']</label></div>\n';
 				}
 				?>
 				<button type="submit" class="btn btn-success">Create Class</button>
@@ -131,7 +131,7 @@
 					<select class="form-control">
 					<?php
 					foreach($data['classes'] as $class){
-					  echo '<option>' . $class . '</option>\n'
+					  echo '<option>' . $class . '</option>\n';
 					}
 					?>
 					</select>
